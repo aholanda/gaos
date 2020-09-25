aec6101be3c4c86abe6515975f823aea670fd527
Adriano Holanda <aholanda@usp.br>
2020-09-25 17:45:39 -0300

    1st commit

---
+++ __main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Main executable for the project used in most
+part to generate graphs from diverse input
+locations.
+'''
+
+# local
+import graphs.linux as lnx
+
+if __name__ == '__main__':
+    lnx.generate_graphs()
