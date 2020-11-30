1dacfd87de1c1db44f5642697e7b656878a0b264
Adriano Holanda <aholanda@usp.br>
2020-11-30 17:04:52 -0300

    add maximum size for atom length

--- atom.h
+++ atom.h
@@ -1,6 +1,8 @@
 #ifndef LIBGRAPHS_ATOM_H
 #define LIBGRAPHS_ATOM_H
 
+#define ATOM_MAXSZ 256
+
 extern char *atom_new(char *str, int len);
 extern char *atom_string(char *str);
 extern void atom_free();
