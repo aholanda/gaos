936aa906f857b34b2dff2afc728a2e3d512147ee
Adriano Holanda <aholanda@usp.br>
2020-12-08 14:55:44 -0300

    GraphBase section fixed

--- gb.c
+++ gb.c
@@ -103,7 +103,7 @@ static void fill_utils (Graph *g, Vertex *v, Arc *a,
                     g->utils[u_idx].I = i;
             break;
             case 'S':
-                s = atom_string(&field[0]);
+                s = atom_string(g->buckets, g->nbuckets, &field[0]);
                 if (v != NULL)
                     v->utils[u_idx].S = s;
                 else if (a != NULL)
@@ -145,6 +145,9 @@ static int get_last_util_idx(Graph *g, int begin, int end) {
     return stop;
 }
 
+static void fwrite_separator(FILE *fp) {
+    fputc(GB_SEP, fp);
+}
 
 static Graph *fill_graph(Graph *g, char data[], Arc *arcs_arr,
                          char *file, int lineno) {
@@ -177,7 +180,7 @@ static Graph *fill_graph(Graph *g, char data[], Arc *arcs_arr,
         }         
 
         if (field_no == 0) {          
-            g->id = atom_string(&buf[0]);
+            g->id = atom_string(g->buckets, g->nbuckets, &buf[0]);
             field_no++;
         } else if (field_no == 1) {
             g->n = atol(&buf[0]);
@@ -233,7 +236,7 @@ static Vertex *fill_vertex(Graph *g, long v_idx, Arc *arcs_arr,
         *p++, buf[i] = '\0'; 
 
         if (field_no == 0) {
-            v->name = atom_string(&buf[0]);
+            v->name = atom_string(g->buckets, g->nbuckets, &buf[0]);
             field_no++;
         } else if (field_no == 1) {
             /* remove the letter V, e.g., V1 */
@@ -390,4 +393,236 @@ Graph *gb_read(char *filename) {
     FCLOSE(fp);
 
     return g;
+}
+
+/*******************************************************************/
+/*                WRITE GRAPH TO FILE                              */
+/*WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW*/
+static void fwrite_arc(FILE *fp, HashMap *a2aidx, Arc **arcs_parr, Arc *a) {
+    Arc *a_idx; 
+
+    a_idx = hashmap_get(a2aidx, a);
+    fprintf(fp, "A%ld",  &a_idx - &arcs_parr[0]);
+}
+
+static void fwrite_vertex(FILE *fp, Graph *g, Vertex *v) {
+    long i;
+
+    i = v - &g->vertices[0];
+    fprintf(fp, "V%ld", i);
+}
+
+static void fwrite_util_value(FILE *fp, Graph *g, char ut, util *pu,
+            HashMap *a2aidx, Arc **arcs_parr) {
+    switch(ut) {
+        case 'A':
+            fwrite_arc(fp, a2aidx, arcs_parr, (*pu).A);
+        break;
+        case 'I':
+            fprintf(fp, "\"%ld\"", (*pu).I);
+        break;
+        case 'S':
+            fprintf(fp, "\"%s\"", (*pu).S);
+        break;
+        case 'V':
+            fwrite_vertex(fp, g, (*pu).V);
+        break;
+        default:
+        break;
+    }
+}
+
+static void fwrite_utils(FILE *fp, Graph*g, Vertex *v, Arc *a, 
+                        HashMap *a2aidx, Arc **arcs_parr) {
+    int u;
+    char ut; /* util type label */
+    util *pu;
+    int offset = 0; /* number of indices to advance */
+    enum section sec = -1;
+
+    if (a != NULL)
+        sec = ARCS;
+    else if (v != NULL)
+        sec = VERTICES;
+    else
+        sec = GRAPHBASE;
+    
+    switch (sec) {
+        case GRAPHBASE:
+            offset = GRAPH_V_UTILS_LEN+GRAPH_A_UTILS_LEN;
+            for (u = offset; u < GRAPH_UTILS_LEN; u++) {
+                ut = g->util_types[u];
+                if (ut != 'Z') {
+                    pu = &g->utils[u - offset];
+                    fwrite_util_value(fp, g, ut, pu, a2aidx, arcs_parr);
+                    fwrite_separator(fp);
+                }
+            }
+        break;
+        case VERTICES:
+            assert(v);
+            offset = 0;
+            for (u = offset; u < GRAPH_A_UTILS_LEN; u++) {
+                ut = g->util_types[u];
+                if (ut != 'Z') {
+                    pu = &v->utils[u - offset];
+                    fwrite_util_value(fp, g, ut, pu, a2aidx, arcs_parr);
+                    fwrite_separator(fp);
+                }
+            }
+        break;
+        case ARCS:
+            assert(a);
+            offset = GRAPH_V_UTILS_LEN;
+            for (u = offset; u < offset + GRAPH_A_UTILS_LEN; u++) {
+                ut = g->util_types[u];
+                if (ut != 'Z') {
+                    pu = &a->utils[u - offset];
+                    fwrite_util_value(fp, g, ut, pu, a2aidx, arcs_parr);
+                    fwrite_separator(fp);
+                }
+            }
+        break;
+        case CHECKSUM:
+#warning checksum not implemented yet
+        break;
+        default:
+            fprintf(stderr, "Unrecognized section %d\n", (int)sec);
+            exit(EXIT_FAILURE);
+        break;        
+    }
+    /* switch the last separator by new line */
+    fseek(fp, -1, SEEK_CUR);
+    fputc('\n', fp);
+}
+
+static void fwrite_graphbase_section(FILE *fp, Graph *g) {
+    int ret = 0;
+    static char buf[GB_MAXLINE];
+    char *p0, *p, *plim; /* pointer to buffer start, current and limit */
+
+    /* 1st line */
+    /* 
+        In the first line we use g->n and g->type what is different
+        from SGB that uses the allocated blocks.
+     */
+    ret = snprintf(&buf[0], GB_MAXLINE, 
+                   "* GraphBase graph (util_types %14s,%ldV,%ldA)\n",
+                   g->util_types, g->n, g->m);
+    fprintf(fp, "%s", &buf[0]);
+    assert(ret > 0);
+
+    /* graph attributes */
+    assert(g->id);    
+    p0 = &g->id[0];
+    plim = p0 + strlen(&g->id[0]);
+    fputc('"', fp); /* start quote */
+    for (p = &g->id[0]; p < plim; p++) {
+        /* continue on the next line*/
+        if ((p - p0) == 58) {
+            fputc('\\', fp);
+            fputc('\n', fp);
+        }
+        fputc(*p, fp);
+    }
+    fputc('"', fp); /* end the quote */    
+    fwrite_separator(fp);
+    fputc('\n', fp); /* put the attributes and util types in a new line */
+
+    /* effective number of vertices */
+    fprintf(fp, "%ld", g->n);
+    fwrite_separator(fp);
+ 
+    /* effective number of arcs */
+    fprintf(fp, "%ld", g->m);    
+    fwrite_separator(fp);
+
+    fwrite_utils(fp, g, NULL, NULL, NULL, NULL);
+    fflush(fp);   
+}
+
+static void fwrite_vertices_section(FILE *fp, Graph *g, HashMap *a2aidx, Arc **arcs_parr) {
+    Vertex *v;
+    Arc *a;
+
+    fprintf(fp, "* %s\n", section_names[VERTICES]);
+    for (v = &g->vertices[0]; v < &g->vertices[0] + g->n; v++) {
+        a = v->arcs;
+        /* Name */
+        if (v->name) {
+            fprintf(fp, "\"%s\"", v->name);
+            fwrite_separator(fp);  
+        } else {
+            fprintf(fp, "\"0\"");
+            fwrite_separator(fp);  
+        }
+
+        if (a == NULL) {
+            /* next arc */
+            fprintf(fp, "0");
+            fwrite_separator(fp);
+        } else {
+            fwrite_arc(fp, a2aidx, arcs_parr, a);
+        }
+        fwrite_utils(fp, g, v, NULL, a2aidx, arcs_parr);
+    }
+}
+
+static void fwrite_arcs_section(FILE *fp, Graph *g, HashMap *a2aidx, Arc **arcs_parr) {
+    Vertex *v = NULL;
+    Arc *a;
+
+    fprintf(fp, "* %s\n", section_names[VERTICES]);
+    for (a = arcs_parr[0]; &a < &arcs_parr[0] + g->m; a++) {
+        a = a->next;
+        if (a == NULL) {
+            fprintf(fp, "0");
+            fwrite_separator(fp);
+            /* Length */
+            fprintf(fp, "0");
+            fwrite_separator(fp);  
+        } else {
+            fwrite_arc(fp, a2aidx, arcs_parr, a);
+            /* Length */
+            fprintf(fp, "%ld", a->len);
+            fwrite_separator(fp);
+        }
+        fwrite_utils(fp, g, v, NULL, a2aidx, arcs_parr);
+    }
+}
+
+void gb_write(Graph *g, char *filename) {
+    FILE *fp;
+    Vertex *v;
+    Arc *a = 0;
+    long acount = 0; /* arc counter */
+    /* Array of pointer to arcs to numbering them */
+    Arc **arcs_parr;
+    /* Map arc pointers to their address in the array of arc pointer */
+    HashMap *a2aidx;
+
+    assert(g);
+    assert(filename);
+
+    FOPEN(fp, filename, "w");
+
+    fwrite_graphbase_section(fp, g);
+    FCLOSE(fp);
+    return;
+
+    a2aidx = hashmap_new(g->m, NULL, NULL);
+    arcs_parr = (Arc **)CALLOC(g->m, sizeof(Arc *));
+    for (v = &g->vertices[0]; v < &g->vertices[0] + g->n; v++)
+        for (a = v->arcs; a; a = a->next) {
+            arcs_parr[acount] = a;
+            hashmap_put(a2aidx, a, &arcs_parr[acount]);
+            acount++;
+        }
+    fwrite_vertices_section(fp, g, a2aidx, arcs_parr);
+
+    fwrite_arcs_section(fp, g, a2aidx, arcs_parr);
+
+    FREE(arcs_parr);
+    hashmap_free(&a2aidx);
+    FCLOSE(fp);
 }
