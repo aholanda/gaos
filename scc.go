0cc5c2e87e40eb180a09d535f97cec8a9d4e0187
Adriano Holanda <aholanda@usp.br>
2020-12-09 15:50:34 -0300

    Algorithm 4.6 from "Algorithms" book

---
+++ scc.go
@@ -0,0 +1,49 @@
+package graphs
+
+// Algorithm 4.6 from "Algorithms" book
+// by Sedgewick and Wayne
+
+type KosarajuSharirSCC struct {
+	visited map[*Vertex]bool
+	id      map[*Vertex]int
+	count   int
+	digraph *Digraph
+}
+
+func KosarajuSharirSCCNew(d *Digraph) *KosarajuSharirSCC {
+	return &KosarajuSharirSCC{
+		visited: make(map[*Vertex]bool),
+		id:      make(map[*Vertex]int),
+		count:   0,
+		digraph: d,
+	}
+}
+
+func (ks *KosarajuSharirSCC) Compute() {
+	var order DepthFirstOrder
+
+	order = DepthFirstOrder(ks.d.Reverse())
+	for v := range order.ReversePost() {
+		if ks.visited[v] == false {
+			ks.dfs(v)
+			ks.count += 1
+		}
+	}
+}
+
+func (ks *KosarajuSharirSCC) dfs(v *Vertex) {
+	var a *Arc
+	var w *Vertex
+
+	ks.visited[v] = true
+	ks.id[v] = ks.count
+
+	for a = v.Arcs; a == nil; a = a.Next {
+		w = a.Tip
+
+		if ks.visited[w] == false {
+			ks.dfs(w)
+		}
+	}
+
+}
