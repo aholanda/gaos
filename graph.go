92132f90960ce80cfa32547a9e0a9c24d814c54e
Adriano Holanda <aholanda@usp.br>
2020-12-22 12:03:34 -0300

    finish average degree calculation and doc

--- graph.go
+++ graph.go
@@ -3,10 +3,18 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
+// Package graphs provides a directed graph data structure (Digraph)
+// and some operations on it, as well a undirected graph (Graph)
+// that is a specialization of Digraph. Using the composition
+// to define Graph, some methods of Digraph may be reused.
+// See the specification
+// [Graphs.tla](https://github.com/aholanda/graphs/blob/master/Graphs.tla)
+// using Temporal Logic of Actions (TLA+).
 package graphs
 
 import (
 	"errors"
+	"math"
 	"strconv"
 )
 
@@ -19,15 +27,35 @@ import (
 // array of adjacencies.
 type VertexID int
 
+// Digraph represents a directed graph identified
+// by ID with N vertices and M arcs where each
+// VertexID v is the index of Adjs and each Adjs[v]
+// has a collection of neighbors represented by their
+// VertexID.
+// Its possible to name each VertexID, but it's
+// optional.
 type Digraph struct {
-	ID           string              // name of the graph
-	Adjs         [][]VertexID        // array of adjacencies
-	N            int                 // number of vertices
-	M            int                 // number of arcs
-	Keys         []string            // store vertices' names
-	NameToVertex map[string]VertexID // names for vertices (optional)
+	// Identification of the graph
+	ID string
+	// Array of adjacencies where each index is a VertexID v
+	// stored sequentially representing where the arc comes from
+	// and in the array of the index v is accumulated
+	// the VertexID w to where the arc goes to.
+	Adjs [][]VertexID
+	N    int // Number of vertices
+	M    int // Number of arcs
+	// Keys array links the VertexID to its name.
+	Keys []string
+	// Map a VertexID to its name.
+	NameToVertex map[string]VertexID
 }
 
+// NewDigraph returns a pointer to a newly create
+// Digraph structure initialized with the nvertices
+// passed as argument to allocate Adjs[] array and
+// to set N.
+// The structure to name vertices are also initialized.
+// A default name is assigned to ID.
 func NewDigraph(nvertices int) *Digraph {
 	d := &Digraph{
 		ID:           "digraph",
@@ -43,14 +71,24 @@ func NewDigraph(nvertices int) *Digraph {
 	return d
 }
 
+// NameIt set a name represented by the argument
+// id to the digraph. The field is called ID to
+// differentiate from vertex name.
 func (d *Digraph) NameIt(id string) {
 	d.ID = id
 }
 
-func (d *Digraph) Name() string {
+// String() is the implementation of the
+// Stringer interface. When the digraph
+// d is printed, this method is used.
+func (d *Digraph) String() string {
 	return d.ID
 }
 
+// VertexIndex returns the VertexID associated with
+// the name passed as argument. An non-nil error is
+// returned if the name does not exists in the map
+// responsible to link vertex's name and its VertexID.
 func (d *Digraph) VertexIndex(name string) (VertexID, error) {
 	if v, ok := d.NameToVertex[name]; ok {
 		return v, nil
@@ -58,6 +96,8 @@ func (d *Digraph) VertexIndex(name string) (VertexID, error) {
 	return -1, errors.New("vertex named " + name + " not found")
 }
 
+// NameVertex assigned a name to VertexID v if it exists
+// in the digraph.
 func (d *Digraph) NameVertex(v VertexID, name string) {
 	if d.HasVertex(v) {
 		if _, ok := d.NameToVertex[name]; !ok {
@@ -67,6 +107,8 @@ func (d *Digraph) NameVertex(v VertexID, name string) {
 	}
 }
 
+// VertexName returns the name assigned to VertexID v,
+// if it exists in the digraph.
 func (d *Digraph) VertexName(v VertexID) string {
 	if d.HasVertex(v) {
 		return d.Keys[v]
@@ -74,21 +116,39 @@ func (d *Digraph) VertexName(v VertexID) string {
 	return "v" + strconv.Itoa(int(v))
 }
 
-// Order of the graph
+// VertexDegree returns the number of neighbors
+// (degree) accumulated in the array d.Adjs[v],
+// if v exists in the digraph.
+func (d *Digraph) VertexDegree(v VertexID) (int, error) {
+	if d.HasVertex(v) {
+		return len(d.Adjs[int(v)]), nil
+	}
+	return -1, errors.New("vertex named " + strconv.Itoa(int(v)) +
+		" not found")
+}
+
+// V returns the number of vertices in the
+// graph also called order of the graph.
 func (d *Digraph) V() int {
 	return d.N
 }
 
-// Size of the graph in arcs
+// A returns the number of arcs in the digraph
+// also called size of the graph.
 func (d *Digraph) A() int {
 	return d.M
 }
 
-// Size of the graph in arcs
+// Size returns the number of arcs in the digraph.
+// It does the same function as A, it was included
+// to conform with the graph theory's literature.
 func (d *Digraph) Size() int {
 	return d.M
 }
 
+// AddArc creates an arc going from v to w.
+// To create an arc VerteXID w in appended
+// in the array d.Adjs[v].
 func (d *Digraph) AddArc(v, w VertexID) error {
 	if d.HasVertex(v) == false {
 		from := strconv.Itoa(int(v))
@@ -116,6 +176,42 @@ func (d *Digraph) HasVertex(v VertexID) bool {
 	return false
 }
 
+// AverageDegree calculates the vertices'
+// average degree of the digraph. It's used
+// the method of provisional means (one-pass algorithm)
+// and it is returned the average and the standard deviation
+// of the vertices' degrees.
+// The algorithm was based on formula expressed in the
+// "The Art of Computer Programming", Vol. 2, 3rd edition,
+// by Don Knuth, page 232.
+func (d *Digraph) AverageDegree() (avg, stdDev float64) {
+	const KOld int = 0 // Represents k-1 index
+	const KNew int = 1 // Represents k index
+	// Average of the first k-1 and k numbers.
+	var avgs = [2]float64{0.0, 0.0}
+	// Stardard deviation of k-1 and k numbers.
+	var stdDevs = [2]float64{0.0, 0.0}
+	var deg int = 0 // Vertex degree
+	var k int = 0   // data index that starts at 1 and not zero.
+
+	deg, _ = d.VertexDegree(0)
+	avgs[KOld] = float64(deg)
+	stdDevs[KOld] = 0.0
+	for i := 1; i < d.N; i++ {
+		k = i + 1
+		deg, _ = d.VertexDegree(VertexID(i))
+		// average
+		avgs[KNew] = avgs[KOld] + (float64(deg)-avgs[KOld])/float64(k)
+		// standard deviation
+		stdDevs[KNew] = stdDevs[KOld] +
+			(float64(deg)-avgs[KOld])*(float64(deg)-avgs[KNew])
+		// Update data indexed by k
+		avgs[KOld] = avgs[KNew]
+		stdDevs[KOld] = stdDevs[KNew]
+	}
+	return avgs[KNew], math.Sqrt(stdDevs[KNew] / float64(k-1))
+}
+
 // Reverse generates a new digraph reverting
 // the direction of all arcs in the Digraph d,
 // the function converts v -> w to w -> v traversing
@@ -131,17 +227,27 @@ func Reverse(d *Digraph) *Digraph {
 	return rev
 }
 
+// Graph is an undirected graph composed by Digraph
+// and a counter for the edges called MM.
 type Graph struct {
 	*Digraph
 	MM int
 }
 
+// NewGraph creates an undirected Graph and
+// returns a pointer to it. The undirected
+// Graof is a Digraph with arcs pointed in
+// both directions for each vertices' neighbors.
 func NewGraph(nvertices int) *Graph {
 	d := &Graph{NewDigraph(nvertices), 0}
 	d.ID = "graph"
 	return d
 }
 
+// AddEdge add 2 arcs pointing to v->w and
+// w->v, forming the edge v-w. To differentiate
+// from Digraph the field g.MM is the counter for
+// edges.
 func (g *Graph) AddEdge(v, w VertexID) {
 	g.AddArc(v, w)
 	if v != w {
@@ -150,11 +256,13 @@ func (g *Graph) AddEdge(v, w VertexID) {
 	g.MM++
 }
 
-// Size of the graph in edges
+// E returns the number of edges in the undirected graph.
 func (g *Graph) E() int {
 	return g.MM
 }
 
+// Size returns the number of edges in the undirected graph.
+// It was implement to conform with graph theory literature.
 func (g *Graph) Size() int {
 	return g.MM
 }
